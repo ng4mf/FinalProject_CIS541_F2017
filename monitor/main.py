21716686db7e43a0350c1c28a269713acfa4d46b
Nikhil Shenoy <nikhilshenoy17@gmail.com>
2017-12-02 11:37:55 -0500

    basic navbar

--- monitor/main.py
+++ monitor/main.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import *
+
+nav = Nav()
+
+@nav.navigation()
+def mynavbar():
+    return Navbar(
+        'Pacemaker Project',
+        View('Dashboard', 'index'),
+        View('About', 'about')
+    )
 
 app = Flask(__name__)
 Bootstrap(app)
+nav.init_app(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', message='About page under construction')
+
+if __name__ == "__main__":
+    app.run(debug=True)
